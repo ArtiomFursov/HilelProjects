6fc8e3d8aa56bb90dfca0c998244376ad9f1f6b0
ArtiomFursov <78692376+ArtiomFursov@users.noreply.github.com>
2022-08-07 23:49:24 +0300

    Proj 11

---
+++ Hw11_Fursov/js/main.js
@@ -0,0 +1,76 @@
+let HAMBURGER = {  
+    price: 0,  
+    calories: 0,  
+
+    small: {  
+     price: 5,  
+     calories: 20  
+    },  
+    large: {  
+     price: 10,  
+     calories: 40  
+    },  
+    toppings:{ 
+        cheese: {  
+            price: 1,  
+            calories: 20  
+           },  
+
+           salad: {  
+            price: 2,  
+            calories: 5  
+           },  
+
+           potato: {  
+            price: 1.5,  
+            calories: 10  
+           } 
+    }, 
+    supplements:{ 
+        seasoning: {  
+            price: 1.5,  
+            calories: 0  
+           },  
+        mayonnaise: {  
+            price: 2,  
+            calories: 5  
+           } 
+    } 
+}; 
+
+class Hamburger{ 
+ constructor() { 
+  // this._size = size; 
+   this.price = [];
+   this.calories = [];
+   // console.log(size) 
+} 
+addToppings([...args]) { 
+  this.args = args 
+  args.map((item) => this.price.push(item.price)), 
+  args.map((item) => this.calories.push(item.calories)) 
+//   console.log(this.price,this.calories) ;     
+} 
+addSupplement(supplement){ 
+  this.supplement = supplement 
+  let res = [];
+  res.push(supplement); 
+  res.map((item) => this.price.push(item.price)) 
+  res.map((item) => this.calories.push(item.calories)) 
+  console.log(this.price +" Supl price") 
+  console.log(this.calories+" Supl cal") 
+} 
+getPrice() {
+    const sum=this.price.reduce((a,b)=>a+b);
+    return 'Price = '+" "+sum ; 
+} 
+getCalories() {
+    const cal = this.calories.reduce((a,b)=>a+b);
+    return 'Calories = '+cal;
+} 
+} 
+let burger = new Hamburger() 
+burger.addToppings([HAMBURGER.small,HAMBURGER.toppings.salad,HAMBURGER.toppings.cheese]) 
+burger.addSupplement(HAMBURGER.supplements.mayonnaise);
+console.log(burger.getPrice());
+console.log(burger.getCalories());
