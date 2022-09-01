11f8b282934eb207c9b0f21983558b6eeac55915
ArtiomFursov <78692376+ArtiomFursov@users.noreply.github.com>
2022-09-01 05:15:58 +0300

    Star Wars

---
+++ SWAPI/main.js
@@ -0,0 +1,84 @@
+$('.container').append('<h2 class="h2">People List</h2>');
+$('.container').append('<ul class="people"></ul>');
+
+const input = $('#input');
+const btn = $('#search');
+let peopleArr =[];
+ for (let i = 1; i < 10; i++) {
+        
+        $.ajax({
+            url: `https://swapi.dev/api/people/?page=${i}`,
+           
+        }).done(function(result){
+            result.results.map(function(el){
+                peopleArr.push(el);
+              })
+        })
+    }
+console.log(peopleArr);
+$(document).ready(function(){
+$('#search').click(function(){
+ if(input.val()!==''){
+peopleArr.map((el)=>{
+    if(el.name.toLowerCase()===input.val().toLowerCase()){
+        $('.people').append(`<li id='${el.name}'><h3 class="${el.name}">${el.name}</h3><span class="${el.eye_color}"></span></li>`);  
+             let str =el.name;
+             str=str.split(' ').join('.');
+             if(el.homeworld){
+                $.ajax({
+                    url: `${el.homeworld}`,
+                }).done(function(result){
+                    // console.log(result.name);
+                    el.homeworld=`${result.name}`;
+                })
+     
+            }
+            // console.log(el.name);
+            Object.entries(el).forEach(([key1,value])=>{
+             if(Array.isArray(value)){
+                el[key1]='';
+                console.log(el[key1]);
+           value.forEach((el1)=>{
+            $.ajax({
+                url:`${el1}`,
+               
+            }).done(function(result){
+               if(result.hasOwnProperty('name')){
+                value=result.name;
+                // console.log(value+": "+key1);
+            if(key1 in el){
+                // console.log('value : '+value+" K1:"+key1);
+                el[key1]+=value+', '.replace(/,(?![^,]*,)/m, '.');
+            }
+          
+               }if(result.hasOwnProperty('title')){
+                // console.log(key1+': '+result.title);
+                if(key1 in el){
+                    el[key1]+=result.title+', '.replace(/,(?![^,]*,)/m, '.');
+                }
+               }
+            })
+           })
+             }
+            })
+        //    console.log(el);
+        $(`.${str}`).one('click' ,function(){
+          
+                Object.entries(el).forEach(([key, value]) => {
+                    $(`span.${el.eye_color}`).append(`<p> ${key} : ${value}</p>`);
+                    $(`span`).click(function(){
+                        $('li').remove();
+                    })
+                    });
+        }) 
+    }
+});
+ };
+});
+});
+
+
+
+
+
+
