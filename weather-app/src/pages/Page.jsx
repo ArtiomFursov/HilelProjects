f589ccce2451075099b8b1a5b4b021038655050b
ArtiomFursov <78692376+ArtiomFursov@users.noreply.github.com>
2022-10-02 09:40:52 +0300

    React weather app

--- weather-app/src/pages/Page.jsx
+++ weather-app/src/pages/Page.jsx
@@ -27,31 +27,32 @@ class Page extends Component {
     }
 
     render() {
-        return (
-            <ul className='pageUl'>
+        if(this.state.cities_List.length>0){
+            return (
+                <ul className='pageUl'>
 
-                { this.state.cities_List.map((el)=>{
-                    return <li key={el.city.id} className='DaysOfWeek'>
-                        <h2 className='PageCityName'>{el.city.name}</h2>
+                    { this.state.cities_List.map((el)=>{
+                        return <li key={el.city.id} className='DaysOfWeek'>
+                            <h2 className='PageCityName'>{el.city.name}</h2>
 
-                        {el.list.map((el)=>{
-                            if(el.dt_txt.includes('18:00:00')){
-                              return <div key={el.main.temp} className='NewPageItem'>
-                                  <h3>{el.dt}</h3>
-                                  <span className='PageWeatherSpan'>temp:{el.main.temp}º</span>
-                                  <span className='PageWeatherSpan'>temp min:{el.main.temp_min}º</span>
-                                  <span className='PageWeatherSpan'>temp max:{el.main.temp_max}º</span>
-                                  <span className='PageWeatherSpan'>wind speed:{el.wind.speed}s</span>
-                                  <span className='PageWeatherSpan'>sea level:{el.main.sea_level}</span>
-                                  <span className='PageWeatherSpan'>ground level:{el.main.grnd_level}</span>
-                                  <span className='PageWeatherSpan'>Feels like:{el.main.feels_like}</span>
-                                  <img src={`http://openweathermap.org/img/wn/${el.weather[0].icon }@2x.png`}></img>
+                            {el.list.map((el)=>{
+                                if(el.dt_txt.includes('18:00:00')){
+                                    return <div key={el.main.temp} className='NewPageItem'>
+                                        <h3>{el.dt}</h3>
+                                        <span className='PageWeatherSpan'>temp:{el.main.temp}º</span>
+                                        <span className='PageWeatherSpan'>temp min:{el.main.temp_min}º</span>
+                                        <span className='PageWeatherSpan'>temp max:{el.main.temp_max}º</span>
+                                        <span className='PageWeatherSpan'>wind speed:{el.wind.speed}s</span>
+                                        <span className='PageWeatherSpan'>sea level:{el.main.sea_level}</span>
+                                        <span className='PageWeatherSpan'>ground level:{el.main.grnd_level}</span>
+                                        <span className='PageWeatherSpan'>Feels like:{el.main.feels_like}</span>
+                                        <img src={`http://openweathermap.org/img/wn/${el.weather[0].icon }@2x.png`}></img>
 
 
-                              </div>
-                            }
-                        })}
-                    </li>
+                                    </div>
+                                }
+                            })}
+                        </li>
 
 
                         // {/*{   el1.list.map((el)=>{*/}
@@ -83,9 +84,13 @@ class Page extends Component {
 
 
 
-            })}
-            </ul>
-        );
+                    })}
+                </ul>
+            );
+        }else{
+            return <h2>This request doesn't exist</h2>
+        }
+
     }
 }
 
